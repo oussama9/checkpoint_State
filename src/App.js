ff7f972571623345aa0168140b8518284d1fdc00
Oussama <guezguezoussama@gmail.com>
2022-05-30 02:20:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import personalImg  from './me.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      show : true,
+      Person : { 
+                fullName : "Oussama Guezguez",
+                bio : "Ingénieur informatique", 
+                imgSrc: personalImg, 
+                profession:"FullStack developer"
+              },
+    }
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+  
+
+
+  clickHandler(){
+    this.setState(function (prevState){
+      return{
+        show : !prevState.show ,
+      }
+    });
+  }
+
+  render(){
+    if(this.state.show)
+    return (
+      <div className="container">
+        <button className="btn btn-primary" onClick={this.clickHandler}>Hide</button>
+        <br/>
+        <img src={personalImg} alt="image peronel" width="400" height="430" />
+        <h1>{this.state.Person.fullName}</h1>
+        <h2>{this.state.Person.bio}</h2>
+        <h2>{this.state.Person.profession}</h2>
+      </div>
+      );
+      else return (
+        <div className="container">
+      <button className="btn btn-primary" onClick={this.clickHandler}>Show</button>
+      </div>
+      );
+  }
 }
 
 export default App;
